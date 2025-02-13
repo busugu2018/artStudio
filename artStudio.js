9627493f1429ca4d13cd8d45c060ee81f45e4b86
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2025-02-13 00:17:40 -0600

    Commit 5: Gallery and About started.

--- artStudio.js
+++ artStudio.js
@@ -13,3 +13,18 @@ document.body.onmousemove = function(e) {
       + 'px'
     );
   }
+
+
+
+  const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("li").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
